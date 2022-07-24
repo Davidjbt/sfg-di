1852960ffdc8a76a344d0da617f9d1241452aa76
David <davidbazanticse@gmail.com>
2022-07-24 22:01:43 +0100

    Saving current files

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -1,13 +1,18 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
+import guru.springframework.sfgdi.repositories.SpanishGreetingRepository;
 
-@Profile({"ES", "default"})
-@Service("i18nService")
+//@Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
+
+    private final SpanishGreetingRepository spanishGreetingRepository;
+
+    public I18nSpanishGreetingService(SpanishGreetingRepository spanishGreetingRepository) {
+        this.spanishGreetingRepository = spanishGreetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
-        return "Hola Mundo - ES";
+        return spanishGreetingRepository.getGreeting();
     }
 }
